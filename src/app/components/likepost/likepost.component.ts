ef646d34ce33c996486da6195c6b5acbe16e45d5
rccurtis <r.c.cars23@gmail.com>
2023-01-06 11:29:01 -0500

    Added likepost component

---
+++ src/app/components/likepost/likepost.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-likepost',
+  templateUrl: './likepost.component.html',
+  styleUrls: ['./likepost.component.css']
+})
+export class LikepostComponent {
+
+}

2d287cc847b85187e9ad834c9de042c1e188eb9e
Joshua Matute <117686909+j-matute@users.noreply.github.com>
2023-01-06 16:27:26 -0500

    Firebase config fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { UserCardComponent } from './components/user-card/user-card.component';
 import { UserInitialsPipe } from './pipes/user-initials.pipe';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {AngularFireModule} from "@angular/fire/compat";
-import {environment} from "../environments/environment";
+
 
 import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
 import { environment } from 'src/environments/environment';
@@ -43,7 +43,7 @@ initializeApp(environment.firebaseConfigNotification);
     ReactiveFormsModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
-    AngularFireModule.initializeApp(environment.firebaseConfigAuth),
+    AngularFireModule.initializeApp(environment.firebaseConfigNotification),
   ],
   providers: [],
   bootstrap: [AppComponent]
